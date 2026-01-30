7e18f538fe9cfcf8fba4bee3e588161f6a0fc602
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-30 13:23:14 +0600

    addad a btn if click the btn change the bg and btn text togol

--- src/components/Countres/country/country.jsx
+++ src/components/Countres/country/country.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./country.css";
 const Country = ({ country }) => {
   //   console.log(country);
+
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    setVisited(!visited);
+    // if (visited) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+  };
+
   const { name, flags, capital, independent } = country;
   return (
-    <div className="country">
+    <div className={`country ${visited && "country-visited"}`}>
       <img src={flags.png} alt="" />
       <h3>name: {name.common} </h3>
       <h4>official name: {name.official}</h4>
       <p>capital : {capital}</p>
       <p>independent : {independent ? "free" : "not free"}</p>
+      <button onClick={handleVisited}>
+        {visited ? "visited" : "not visited"}
+      </button>
     </div>
   );
 };
